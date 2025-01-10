dc8fe8174e1de4c3b8c8a417498d43a615c1ea80
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-10 16:38:45 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ function rank() {
 	"author": {
             "@type": "Person",
             "name": "Hbtoto : Slot Bonus New Member 100 Di Awal Tanpa Deposit Dan Slot Bonus 100 Di Depan",
-            "reviewBody": "Hbtoto : Slot Bonus New Member 100 Di Awal Tanpa Deposit Dan Slot Bonus 100 Di Depan, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!"
+            "reviewBody": "Hbtoto menawarkan pengalaman bermain slot yang luar biasa dengan memberikan bonus yang menguntungkan untuk para pemain baru. Sebagai anggota baru, Anda dapat menikmati Slot Bonus New Member 100 di Awal Tanpa Deposit, yang memungkinkan Anda untuk langsung merasakan keseruan bermain tanpa perlu melakukan deposit terlebih dahulu"
           }
         },
     "offers": {
@@ -47,6 +47,22 @@ function rank() {
     };
   }
 
+function histats() {
+    return {
+      __html: `
+        var _Hasync= _Hasync|| [];
+        _Hasync.push(['Histats.start', '1,4921005,4,5,172,25,00010000']);
+        _Hasync.push(['Histats.fasi', '1']);
+        _Hasync.push(['Histats.track_hits', '']);
+        (function() {
+        var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+        hs.src = ('//s10.histats.com/js15_as.js');
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+        })();
+      `,
+    };
+  };
+
 export default function RootLayout({
   children
 }: Readonly<{
@@ -55,7 +71,6 @@ export default function RootLayout({
   return (
     <html lang="id" data-browser="chrome">
       <head>
-        <meta charSet="utf-8" />
         <title>Hbtoto : Slot Bonus New Member 100 Di Awal Tanpa Deposit Dan Slot Bonus 100 Di Depan</title>
         <meta name="description" content="Hbtoto menawarkan pengalaman bermain slot yang luar biasa dengan memberikan bonus yang menguntungkan untuk para pemain baru. Sebagai anggota baru, Anda dapat menikmati Slot Bonus New Member 100 di Awal Tanpa Deposit, yang memungkinkan Anda untuk langsung merasakan keseruan bermain tanpa perlu melakukan deposit terlebih dahulu" />
         <meta name="keywords" content="Hbtoto, Slot Bonus New Member 100, Deposit Dan Slot Bonus 100, Situs Judi Online Terpercaya, Deposit Via Dana, Link Alternatif Slot Online, Link Alternatif Slot, Slot Online Deposit Pulsa, Bandarqq Alternatif" />
@@ -80,7 +95,7 @@ export default function RootLayout({
         <meta name="distribution" content="global" />
         <meta name="format-detection" content="telephone=no, address=no, email=no" />
         <meta property="og:title" content="Hbtoto : Slot Bonus New Member 100 Di Awal Tanpa Deposit Dan Slot Bonus 100 Di Depan" />
-        <meta property="og:description" content="Hbtoto : Slot Bonus New Member 100 Di Awal Tanpa Deposit Dan Slot Bonus 100 Di Depan, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta property="og:description" content="Hbtoto menawarkan pengalaman bermain slot yang luar biasa dengan memberikan bonus yang menguntungkan untuk para pemain baru. Sebagai anggota baru, Anda dapat menikmati Slot Bonus New Member 100 di Awal Tanpa Deposit, yang memungkinkan Anda untuk langsung merasakan keseruan bermain tanpa perlu melakukan deposit terlebih dahulu" />
         <meta property="og:url" content={nameDomain} />
         <meta property="og:site_name" content="hbtoto" />
         <meta property="og:locale" content="id_ID" />
@@ -91,12 +106,12 @@ export default function RootLayout({
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="hbtoto" />
         <meta name="twitter:title" content="Hbtoto : Slot Bonus New Member 100 Di Awal Tanpa Deposit Dan Slot Bonus 100 Di Depan" />
-        <meta name="twitter:description" content="Hbtoto : Slot Bonus New Member 100 Di Awal Tanpa Deposit Dan Slot Bonus 100 Di Depan, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta name="twitter:description" content="Hbtoto menawarkan pengalaman bermain slot yang luar biasa dengan memberikan bonus yang menguntungkan untuk para pemain baru. Sebagai anggota baru, Anda dapat menikmati Slot Bonus New Member 100 di Awal Tanpa Deposit, yang memungkinkan Anda untuk langsung merasakan keseruan bermain tanpa perlu melakukan deposit terlebih dahulu" />
         <meta name="twitter:image" content={nameDomain} />
         <link rel="icon" href="/favicon.ico?favicon.088e6c96.ico" sizes="256x256" type="image/x-icon" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="next-size-adjust" content="" />
         <script type="application/ld+json" dangerouslySetInnerHTML={rank()} key="product-jsonld" />
+        <script dangerouslySetInnerHTML={histats()} type="text/javascript" />
       </head>
       <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
